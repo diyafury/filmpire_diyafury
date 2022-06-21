775118c66ff922439fd0b1c968eafcdb5bd5dd4c
Diyafury <diyafury@googlemail.com>
2022-06-21 15:54:32 +0200

    Added alan and featured image

---
+++ src/components/Alan.jsx
@@ -0,0 +1,52 @@
+import React, { useEffect, useContext } from 'react';
+import alanBtn from '@alan-ai/alan-sdk-web';
+import { useDispatch } from 'react-redux';
+import { useHistory } from 'react-router-dom';
+
+import { searchMovie, selectGenreOrCategory } from '../features/currentGenreOrCategory';
+import { ColourModeContext } from '../utils/ToggleColourMode';
+import { fetchToken } from '../utils';
+
+const useAlan = () => {
+  const { setMode } = useContext(ColourModeContext);
+  const dispatch = useDispatch();
+  const history = useHistory();
+
+  useEffect(() => {
+    alanBtn({
+      key: 'f61a1641b63de7382008a80b49f589532e956eca572e1d8b807a3e2338fdd0dc/stage',
+      onCommand: ({ command, mode, genres, genreOrCategory, query }) => {
+        if (command === 'ChooseGenre') {
+          const foundGenre = genres.find((g) => g.name.toLowerCase() === genreOrCategory.toLowerCase());
+
+          if (foundGenre) {
+            history.push('/');
+            dispatch(selectGenreOrCategory(foundGenre.id));
+          } else {
+            const category = genreOrCategory.startsWith('top') ? 'top_rated' : genreOrCategory;
+            history.push('/');
+            dispatch(selectGenreOrCategory(category));
+          }
+        } else if (command === 'changeMode') {
+          if (mode === 'light') {
+            setMode('light');
+          } else {
+            setMode('dark');
+          }
+        } else if (command === 'login') {
+          fetchToken();
+        } else if (command === 'logout') {
+          localStorage.clear();
+
+          // window.location.href = '/ ';
+          history.push('/');
+        } else if (command === 'search') {
+          dispatch(searchMovie(query));
+        }
+      },
+    });
+  }, []);
+  // no return needed, is a hook not a component
+};
+
+export default useAlan;
